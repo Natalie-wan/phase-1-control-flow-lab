2cd76a4b0b1a79c5f622547fa08e809c74ab59af
Natalie <wanjikunatalie631@gmail.com>
2024-11-28 08:24:06 +0300

    selection using conditions

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  let charm;
+  switch (tip) {
+    case "generous":
+      charm = 'Thank you so much.';
+    break;
+    case "not as generous":
+      charm = 'Thank you.';
+    break; 
+    default:
+      charm = 'Bye.'; 
+  }
+  return charm;
+};
